e751fa6184fcacec96af4fbde73c0bb4035cfa69
itsdorosh <andry.dorosh@gmail.com>
2022-12-26 20:21:27 +0200

    remove ngOnDestroy check and saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
-import { DataService } from './data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   title = "ng-shaper";
   subtitle = "Make your own 3D space with different figures";
-
-  constructor(public dataService: DataService) {}
-
-  ngOnDestroy(): void {
-    confirm('Would you like to save your latest draft?') && this.dataService.stashData();
-  }
 }
